100cc00fe556a7fe6060f5809eee6173e52fa9fb
Miles Crighton <miles.crighton@gmail.com>
2021-03-22 18:00:53 +0000

    Symlink files script

--- link.sh
+++ link.sh
@@ -1,8 +1,19 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
 # Variables for source and dest paths
 set --local home ~
 set --local src (pwd)
 
-# Symlink /.config
-ln -s $src/.config/* $home
+# Symlink /.config files to home (not dirs)
+ln -s $src/.config/. $home
+
+# For neovim
+mkdir -p $home/.config/nvim
+ln -s .config/nvim/* $home/.config/nvim
+
+# For vim - requires symlinking site and nvim files to ~/.vim
+mkdir -p $home/.local/share/nvim/site
+mkdir -p $home/.vim
+ln -s $home/.local/share/nvim/site $home/.vim
+ln -s .config/nvim/* $home/.vim
+ln -s .config/nvim/init.vim $home/.vimrc
--- readme.md
+++ readme.md
@@ -2,4 +2,5 @@
 
 1. Get XCode command line tools: `xcode-select —install`
 2. Run `setup/brew.sh`
-3. Use `./link.sh` to move any config files to required location
+3. Run `setup/vim.sh`
+4. Use `./link.sh` to move any config files to required location
