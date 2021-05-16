83d0a94578e400cbf05379d67470b98b7192e54e
Miles Crighton <miles.crighton@gmail.com>
2021-05-16 17:51:49 +0100

    Fix symlink sync for .config files

--- link.sh
+++ link.sh
@@ -5,15 +5,4 @@ set --local home ~
 set --local src (pwd)
 
 # Symlink /.config files to home (not dirs)
-ln -s $src/.config/. $home
-
-# For neovim
-mkdir -p $home/.config/nvim
-ln -s $src/.config/nvim/* $home/.config/nvim
-
-# For vim - requires symlinking site and nvim files to ~/.vim
-mkdir -p $home/.local/share/nvim/site
-mkdir -p $home/.vim
-ln -s $home/.local/share/nvim/site $home/.vim
-ln -s $src/.config/nvim/* $home/.vim
-ln -s $src/.config/nvim/init.vim $home/.vimrc
+ln -s $src/.config/.* $home
