14c5aba822933dbc90b2ea77f4f9594c5920ec8d
Miles Crighton <miles.crighton@gmail.com>
2023-01-18 15:09:59 +0000

    Add various git aliases

--- zsh/.config/zsh/include/aliases.zsh
+++ zsh/.config/zsh/include/aliases.zsh
@@ -5,4 +5,10 @@ alias cat='bat'
 alias src='source ~/.zshrc'
 
 ##### Git #####
+alias gs='git status'
 alias gd='git diff'
+alias gl='git l'
+alias gaa='git add -A'
+alias gac='git ac'
+alias gca='git commit --amend'
+alias gcan='git commit --amend --no-edit'
