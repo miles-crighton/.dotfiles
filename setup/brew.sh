171e3f7735d25a398b6e5a248def5f600c1011f9
Miles Crighton <miles.crighton@gmail.com>
2025-01-20 11:50:19 +0000

    Remove shell changes from brew script

--- setup/brew.sh
+++ setup/brew.sh
@@ -9,8 +9,3 @@ brew update
 # Get packages & casks
 xargs brew install < packages.txt
 xargs brew install --cask < casks.txt
-
-# Add homebrew bash & fish to /etc/shells & use fish
-grep '/usr/local/bin/bash' /etc/shells >/dev/null || echo '/usr/local/bin/bash' | sudo tee -a /etc/shells
-grep '/usr/local/bin/fish' /etc/shells >/dev/null || echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
